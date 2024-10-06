a784490261c36c0be50e8a02521f1c950eb009dc
Frédéric Leroux <leroux_frederic33@hotmail.com>
2024-10-05 22:23:35 -0400

    Replace pymesh with trimesh

--- helpers/mesh.py
+++ helpers/mesh.py
@@ -1,16 +1,16 @@
 import numpy as np
-import pymesh
+import trimesh as tm
 import pyvista as pv
 
-def to_pymesh(mesh):
-    """Returns a pymesh from a pyvista PolyData"""
+def to_trimesh(mesh):
+    """Returns a trimesh from a pyvista PolyData"""
     v = mesh.points
     f = mesh.faces.reshape(-1, 4)[:, 1:]
 
-    return pymesh.form_mesh(v, f)
+    return tm.Trimesh(v, f)
 
 def to_pyvista(mesh):
-    """Return a PolyData from a pymesh"""
+    """Return a PolyData from a trimesh"""
     v = mesh.vertices
     f = mesh.faces
     
@@ -18,17 +18,25 @@ def to_pyvista(mesh):
     
     return pv.PolyData(v, f, len(mesh.faces))
 
-def intersect(mesh1, mesh2, engine="igl"):
-    """Returns the intersection of two meshes (in pymesh format)"""
+def intersect(mesh1, mesh2, engine="manifold"):
+    """Returns the intersection of two meshes (in trimesh format)"""
 
-    return pymesh.boolean(mesh1, mesh2, operation="intersection", engine=engine)
+    return tm.boolean.intersection([mesh1, mesh2], engine=engine)
+    
+def union(mesh1, mesh2, engine="manifold"):
+    """Returns the union of two meshes (in trimesh format)"""
 
-def symmetric_difference(mesh1, mesh2, engine="igl"):
-    """Returns the symmetric difference of two volumes (in pymesh format)"""
+    return tm.boolean.union([mesh1, mesh2], engine=engine)
 
-    return pymesh.boolean(mesh1, mesh2, operation="symmetric_difference", engine=engine)
+def difference(mesh1, mesh2, engine="manifold"):
+    """Returns the difference between two volumes (in trimesh format)"""
 
-def difference(mesh1, mesh2, engine="igl"):
-    """Returns the difference between two volumes (in pymesh format)"""
+    return tm.boolean.difference([mesh1, mesh2], engine=engine)
 
-    return pymesh.boolean(mesh1, mesh2, operation="difference", engine=engine)
+def symmetric_difference(mesh1, mesh2, engine="manifold"):
+    """Returns the symmetric difference of two volumes (in trimesh format)"""
+
+    u = union(mesh1, mesh2, engine)
+    i = intersect(mesh1, mesh2, engine)
+
+    return difference(u, i, engine)
